b0ccb962e88ba7b7529184e892b7a0aeb4f32054
Niklas <niklas.nordmann@smail.inf.h-brs.de>
2020-08-30 15:41:42 +0200

    do proper ruler cleanup so it does not stay on screen when releasing ctrl and pressing a mouse button

--- scripts/TokenHook.js
+++ scripts/TokenHook.js
@@ -1,26 +1,38 @@
 export function Patch_Drag() {
-
-    let pushedKey = false;
+    const oldClear = canvas.controls.ruler.clear;
+    canvas.controls.ruler.clear = function () {
+        oldClear.apply(this, arguments);
+        if (ctrlReleased) {
+            canvas.app.stage.removeListener('pointermove', handleMouseMove);
+            canvas.app.stage.removeListener('pointerdown', handleLeftClick);
+            ctrlPushed = ctrlReleased = false;
+        }
+    };
+    let ctrlPushed = false;
+    let ctrlReleased = false;
     const oldKeyEvent = KeyboardManager.prototype.getKey;
     KeyboardManager.prototype.getKey = function (e) {
-        if (e.ctrlKey){
-            pushedKey = true;
-
+        if (e.ctrlKey && !ctrlPushed) {
+            canvas.app.stage.addListener('pointermove', handleMouseMove);
+            canvas.app.stage.addListener('pointerdown', handleLeftClick);
+            ctrlPushed = true;
+            ctrlReleased = false;
             drawFromToken(e);
         }
+
+        ctrlReleased = !e.ctrlKey;
         return oldKeyEvent.apply(this, arguments);
     };
+    const handleMouseMove = event => {
+        drawFromToken(event);
+    };
 
-    const oldLeftMouse = canvas.controls.ruler._onClickLeft;
-    canvas.controls.ruler._onClickLeft = function (e) {
-        oldLeftMouse.apply(this,arguments);
-        pushedKey = false;
-    }
+    const handleLeftClick = () => {
+        if (ctrlReleased) canvas.controls.ruler.clear();
+    };
 }
 
 function drawFromToken(e) {
-
-
     let token = canvas.tokens.controlled['0'];
     if (token === undefined) return;
 
