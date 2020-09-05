0cc59d1cd378ca998849e72df00e513caeb2e635
Nordiii <niklas907@gmail.com>
2020-09-05 12:09:08 +0200

    sync difficult terrain waypoints between players

--- scripts/ruler-hook.js
+++ scripts/ruler-hook.js
@@ -4,12 +4,22 @@ class waypoint {
     }
 }
 
+class detailedWaypointData {
+    constructor(startPoint, endPoint, difficultWaypoints, isDifficultNow) {
+        this.endPoint = endPoint;
+        this.difficultWaypoints = difficultWaypoints;
+        this.isDifficultNow = isDifficultNow;
+        this.startPoint = startPoint;
+    }
+}
+
 export function patchRuler() {
     let ctrlPushed = false;
     let ctrlReleased = false;
     let isDifficultTerrain = false;
     let keyPushedLast = Date.now();
     let waypoints = [];
+    let allWaypoints = new Map();
     let difficultTerrainMultiplier = game.settings.get("rulerfromtoken", "diffTerrainMultiplier");
 
     const oldClear = canvas.controls.ruler.clear;
@@ -22,14 +32,52 @@ export function patchRuler() {
         waypoints.push(new waypoint(isDifficultTerrain));
     };
 
+    const oldBoradcast = game.user.broadcastActivity
+    game.user.broadcastActivity = function (activityData) {
+        if (activityData.ruler !== null) {
+            let token = canvas.tokens.controlled['0'];
+            if (token === undefined) return oldBoradcast.apply(this, arguments);
+            if (activityData.ruler.waypoints[0] === token.center) return oldBoradcast.apply(this, arguments);
+
+            activityData.ruler = Object.assign({
+                difficultTerrain: waypoints,
+                isCurrentlyDifficult: isDifficultTerrain
+            }, activityData.ruler)
+        }
+        oldBoradcast.apply(this, arguments);
+    }
+
+    const oldRulerUpdate = canvas.controls.updateRuler;
+    canvas.controls.updateRuler = function (user, ruler) {
+        if (ruler !== null)
+            allWaypoints.set(user.id, new detailedWaypointData(ruler.waypoints[0], ruler.destination, ruler.difficultTerrain, ruler.isCurrentlyDifficult))
+        oldRulerUpdate.apply(this, arguments)
+    }
+
     const oldHexDist = HexagonalGrid.prototype.measureDistances;
     HexagonalGrid.prototype.measureDistances = function (segments, options = {}) {
+        let currentWaypoints = null;
+        let currentlyDifficult;
+
+        allWaypoints.forEach(value => {
+            if (value.startPoint.x === segments[0].ray.A.x && value.startPoint.y === segments[0].ray.A.y
+                && value.endPoint.x === segments[segments.length - 1].ray.B.x && value.endPoint.y === segments[segments.length - 1].ray.B.y){
+                currentWaypoints = value.difficultWaypoints;
+                currentlyDifficult = value.isDifficultNow
+            }
+        });
         let token = canvas.tokens.controlled['0'];
-        if (token !== undefined) {
+        if (token !== undefined && currentWaypoints === null) {
             if (segments.length === 0 || (segments[0].ray.A.x !== token.center.x || segments[0].ray.A.y !== token.center.y)) {
                 return oldHexDist.apply(this, arguments)
             }
         }
+
+        if (currentWaypoints === null){
+            currentWaypoints = waypoints;
+            currentlyDifficult = isDifficultTerrain
+        }
+
         return segments.map((s, i) => {
             let r = s.ray;
             let [r0, c0] = this.getGridPositionFromPixels(r.A.x, r.A.y);
@@ -40,8 +88,8 @@ export function patchRuler() {
             let hex1 = this._offsetToCube(r1, c1);
             let distance = this._cubeDistance(hex0, hex1);
             if (waypoints.length <= i)
-                return distance * canvas.dimensions.distance * (isDifficultTerrain ? difficultTerrainMultiplier : 1);
-            return distance * canvas.dimensions.distance * (waypoints[i].difficultTerrain ? difficultTerrainMultiplier : 1);
+                return distance * canvas.dimensions.distance * (currentlyDifficult ? difficultTerrainMultiplier : 1);
+            return distance * canvas.dimensions.distance * (currentWaypoints[i].difficultTerrain ? difficultTerrainMultiplier : 1);
 
         });
     };
@@ -49,11 +97,26 @@ export function patchRuler() {
 
     const oldSquareDist = SquareGrid.prototype.measureDistances;
     SquareGrid.prototype.measureDistances = function (segments, options = {}) {
+        let currentWaypoints = null;
+        let currentlyDifficult;
+
+        allWaypoints.forEach(value => {
+            if (value.startPoint.x === segments[0].ray.A.x && value.startPoint.y === segments[0].ray.A.y
+                && value.endPoint.x === segments[segments.length - 1].ray.B.x && value.endPoint.y === segments[segments.length - 1].ray.B.y){
+                currentWaypoints = value.difficultWaypoints;
+                currentlyDifficult = value.isDifficultNow
+            }
+        });
         let token = canvas.tokens.controlled['0'];
-        if (token !== undefined) {
+        if (token !== undefined && currentWaypoints === null) {
             if (segments.length === 0 || (segments[0].ray.A.x !== token.center.x || segments[0].ray.A.y !== token.center.y)) {
                 return oldSquareDist.apply(this, arguments)
             }
+        }
+
+        if (currentWaypoints === null){
+            currentWaypoints = waypoints;
+            currentlyDifficult = isDifficultTerrain
         }
 
         //Basically the original function just with difficult terrain factored in, this will probably break other modules using rulers
@@ -68,9 +131,9 @@ export function patchRuler() {
             let ns = Math.abs(ny - nx);
 
             // Linear distance for all moves
-            if (waypoints.length <= i)
-                return (nd + ns) * d.distance * (isDifficultTerrain ? difficultTerrainMultiplier : 1);
-            return (nd + ns) * d.distance * (waypoints[i].difficultTerrain ? difficultTerrainMultiplier : 1);
+            if (currentWaypoints.length <= i)
+                return (nd + ns) * d.distance * (currentlyDifficult ? difficultTerrainMultiplier : 1);
+            return (nd + ns) * d.distance * (currentWaypoints[i].difficultTerrain ? difficultTerrainMultiplier : 1);
         });
     };
 
